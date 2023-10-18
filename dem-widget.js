e3d62d58082e765b53cb228d96b6a094160ce57b
Johan-Greyling-Cauchy <jo.greyling@gmail.com>
2023-10-18 14:00:51 +0200

    Create dem-widget.js

---
+++ dem-widget.js
@@ -0,0 +1,18 @@
+// Access the container div by its id
+var widgetContainer = document.getElementById('your-widget-container');
+
+// Create and render the widget content within the container
+var widgetContent = document.createElement('div');
+widgetContent.innerHTML = 'Your widget content here';
+widgetContainer.appendChild(widgetContent);
+
+// You can also load additional JavaScript and CSS for the widget dynamically if needed
+var widgetScript = document.createElement('script');
+widgetScript.src = 'path/to/your-widget.js';
+document.head.appendChild(widgetScript);
+
+// Include widget-specific CSS styles if necessary
+var widgetStyles = document.createElement('link');
+widgetStyles.rel = 'stylesheet';
+widgetStyles.href = 'path/to/your-widget-styles.css';
+document.head.appendChild(widgetStyles);
