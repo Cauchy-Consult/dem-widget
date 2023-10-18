b1b0f2e41dbe0488b0469ef25d2e595d2771abba
Johan-Greyling-Cauchy <jo.greyling@gmail.com>
2023-10-18 14:19:25 +0200

    changes testing

--- dem-widget.js
+++ dem-widget.js
@@ -1,19 +1,22 @@
-// Access the container div by its id
-var widgetContainer = document.getElementById('dem-widget-container');
+console.log("is my js executing??")
 
-// Create and render the widget content within the container
-var widgetContent = document.createElement('div');
-widgetContent.innerHTML = '<button class="dem-widget-btn" role="button">Yay les go!</button>';
-widgetContainer.appendChild(widgetContent);
+document.addEventListener('DOMContentLoaded', function () {
+    // Access the container div by its id
+    var widgetContainer = document.getElementById('dem-widget-container');
+    console.log("DOMLOADED?")
+    // Create and render the widget content within the container
+    var widgetContent = document.createElement('div');
+    widgetContent.innerHTML = '<button class="dem-widget-btn" role="button">Yay, let's go!</button>';
+    widgetContainer.appendChild(widgetContent);
 
-// You can also load additional JavaScript and CSS for the widget dynamically if needed
-// var widgetScript = document.createElement('script');
-// widgetScript.src = 'path/to/your-widget.js';
-// document.head.appendChild(widgetScript);
+    // You can also load additional JavaScript and CSS for the widget dynamically if needed
+    // var widgetScript = document.createElement('script');
+    // widgetScript.src = 'path/to/your-widget.js';
+    // document.head.appendChild(widgetScript);
 
-// Include widget-specific CSS styles if necessary
-var widgetStyles = document.createElement('link');
-widgetStyles.rel = 'stylesheet';
-widgetStyles.href = 'https://raw.githubusercontent.com/Cauchy-Consult/dem-widget/test-tag-1.1/dem-widget.css';
-
-document.head.appendChild(widgetStyles);
+    // Include widget-specific CSS styles
+    var widgetStyles = document.createElement('link');
+    widgetStyles.rel = 'stylesheet';
+    widgetStyles.href = 'https://raw.githubusercontent.com/Cauchy-Consult/dem-widget/test-tag-1.2/dem-widget.css';
+    document.head.appendChild(widgetStyles);
+});
