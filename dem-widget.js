030a2b49040d80fc93fb8950d0b2914edb46b2aa
Johan-Greyling-Cauchy <jo.greyling@gmail.com>
2023-10-18 14:52:42 +0200

    local testing

--- dem-widget.js
+++ dem-widget.js
@@ -1,11 +1,10 @@
-console.log("is my js executing??")
-
 document.addEventListener('DOMContentLoaded', function () {
     // Access the container div by its id
     var widgetContainer = document.getElementById('dem-widget-container');
     console.log("DOMLOADED?")
     // Create and render the widget content within the container
     var widgetContent = document.createElement('div');
+    widgetContent.className = 'dem-widget-btn-container'
     widgetContent.innerHTML = '<button class="dem-widget-btn" role="button">Yay, lets go!</button>';
     widgetContainer.appendChild(widgetContent);
 
@@ -17,6 +16,6 @@ document.addEventListener('DOMContentLoaded', function () {
     // Include widget-specific CSS styles
     var widgetStyles = document.createElement('link');
     widgetStyles.rel = 'stylesheet';
-    widgetStyles.href = 'https://raw.githubusercontent.com/Cauchy-Consult/dem-widget/test-tag-1.3/dem-widget.css';
+    widgetStyles.href = 'dem-widget.css';
     document.head.appendChild(widgetStyles);
 });
